b3273e350541dc9ade831a542691e5167d7ab191
Teddy Weisman <theodore.weisman@nist.gov>
2013-07-05 12:35:37 -0400

    Global coordinate frame can be set as a parameter and rotated

--- usarsim_tools/src/base_transform.cpp
+++ usarsim_tools/src/base_transform.cpp
@@ -1,21 +1,31 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <string>
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "base_transform");
 	ros::NodeHandle nh;
+	
+	std::string globalFrame;
+	if(!nh.getParam("usarsim/globalFrame",globalFrame))
+	{
+	  ROS_WARN("usarsim/globalFrame not specified. Using default global frame.");
+	  exit(1);
+	}
 	ros::Rate r(2.0);
 	tf::TransformBroadcaster broadcaster;
 	tf::Transform transform;
 	transform.setOrigin(tf::Vector3(0,0,0));
-	tf::Quaternion quat;
-	quat.setEuler(3.14159, 0, 0);
-	transform.setRotation(quat);
-	ROS_ERROR("ROS Version is %d %d %d",ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH);
+	//reflect y and z axes
+	btMatrix3x3 rotationMatrix(
+	  1,  0,  0,
+	  0, -1,  0,
+	  0,  0, -1);
+	transform.setBasis(rotationMatrix);
 	while(ros::ok())
 	{
-		broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "rviz_base", "odom"));
+		broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), globalFrame, "odom"));
 		r.sleep();
 	}
 	return 0;
